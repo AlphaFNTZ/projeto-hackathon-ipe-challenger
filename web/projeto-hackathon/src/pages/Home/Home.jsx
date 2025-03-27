cccc6afcfddc05bd7341b03a08b3b93b635603e9
Juliano Cesar <julianocesarf.ramos@gmail.com>
2025-03-27 17:56:13 -0300

    Criando o codigo base

---
+++ web/projeto-hackathon/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Home.module.css";
+
+function Home() {
+    return (
+        <div className={style.container}>
+            <h1>
+                E que comece os jogos !!!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
